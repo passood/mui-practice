0457efea63168347daa82c2987606154105d87d2
kim <passood@naver.com>
2022-10-18 12:22:20 +0900

    test4

--- src/components/appbar/appbarDesktop.js
+++ src/components/appbar/appbarDesktop.js
@@ -27,7 +27,7 @@ export default function AppbarDesktop({ matches }) {
 
   return (
     <AppbarContainer>
-      <AppbarHeader variant="h4">My Bagssssss</AppbarHeader>
+      <AppbarHeader variant="h4">My Bagssssssaaaa</AppbarHeader>
       <MyList type="row">
         <ListItemText primary="Home" />
         <ListItemText primary="Categories" />
