fdf5d987513429938bc4070aaf16f6b30257cdd1
kim <passood@naver.com>
2022-10-18 12:18:41 +0900

    title change

--- src/components/appbar/appbarDesktop.js
+++ src/components/appbar/appbarDesktop.js
@@ -27,7 +27,7 @@ export default function AppbarDesktop({ matches }) {
 
   return (
     <AppbarContainer>
-      <AppbarHeader variant="h4">My Bags</AppbarHeader>
+      <AppbarHeader variant="h4">My Bagsaaaaaa</AppbarHeader>
       <MyList type="row">
         <ListItemText primary="Home" />
         <ListItemText primary="Categories" />
