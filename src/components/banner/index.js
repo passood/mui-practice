0ab25a8d6c55ed29dca64356f38b7752b0a0c88e
kim <passood@naver.com>
2022-11-08 10:52:42 +0900

    메인 브랜치 제목 수정

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -19,7 +19,7 @@ export default function Banner() {
       <BannerContent>
         <Typography variant="h6">Huge Collection</Typography>
         <BannerTitle variant="h2">
-          New Bagsss
+          New Bags
         </BannerTitle>
 
         <BannerDescription variant="subtitle">
